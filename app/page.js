c98ea2e5b2add096d8c6d11162c76c28f83f3721
insomeniac176 <insomeniac176@gmail.com>
2024-09-25 11:08:33 +0000

    feat: login function

--- app/page.js
+++ app/page.js
@@ -1,33 +1,61 @@
+import { useState } from 'react';
+import axios from 'axios';
 import Link from 'next/link'
 
 export default function Home() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`https://msg-api-eight.vercel.app/api/users/login`, { username, password });
+      // const response = await axios.post(`${import.meta.env.VITE_API_URL}/users/login`, { username, password });
+
+      const { token } = response.data;
+      localStorage.setItem('token', token); // Simpan token di localStorage
+      navigate('/app');
+    } catch (error) {
+      console.error('Error:', error.response?.data);
+      setError(error.response?.data?.message || 'Error logging in');
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-950">
       <div className="bg-gray-800 p-6 rounded-lg w-full max-w-md border-solid border-slate-400 border">
         <h1 className="text-2xl text-white font-bold">Login</h1>
-        {/* <span className="text-red-300">error</span> */}
+        {error && <span className="text-red-300">{error}</span> }
         <hr className="mb-4 border-slate-400" />
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-white">Username</label>
             <input
               type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               className="w-full p-2 bg-transparent text-white border border-gray-300 rounded-lg"
+              required
             />
           </div>
           <div className="mb-6">
             <label className="block  text-white">Password</label>
             <input
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="w-full p-2 border bg-transparent text-white border-gray-300 rounded-lg"
+              required
             />
           </div>
           <button className="w-full text-indigo-400 font-semibold py-2 px-4 rounded-lg bg-indigo-600 bg-opacity-30 hover:bg-opacity-50 bg-blur-md border-indigo-600 border transition duration-300">
             Login
           </button>
-          <div class="mt-2 text-white text-start">
-            <p class="text-sm">Don&apos;t have an account? 
-              <Link href="/signup" class="text-blue-500 hover:text-blue-700"> Sign up now.</Link>
+          <div className="mt-2 text-white text-start">
+            <p classNane="text-sm">Don&apos;t have an account? 
+              <Link href="/signup" className="text-blue-500 hover:text-blue-700"> Sign up now.</Link>
             </p>
           </div>
         </form>
